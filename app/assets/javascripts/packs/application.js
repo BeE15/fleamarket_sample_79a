276f26eff25b82b5b70e542098cdb753b30b28ad
BeE15 <sotaimo15@gmail.com>
2021-03-23 18:19:39 +0900

    Update application.js

--- app/assets/javascripts/packs/application.js
+++ app/assets/javascripts/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-// require("../create_token");
+require("../category");
 // require("../item_price");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
