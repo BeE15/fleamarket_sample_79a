44fa4ece8acc1c0875d5ac7224286cf6d624513b
BeE15 <sotaimo15@gmail.com>
2021-03-23 18:34:15 +0900

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,11 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require("../category");
-// require("../item_price");
+require("../dropdown_menu");
+require("../products");
+require("../payjp");
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
